10f8d50bdecb3457dd85ba6fd3d2af6aff7a55b0
annasanchez27 <annasanchezespunyes27@gmail.com>
2021-05-14 11:45:48 +0200

    working speaker encoder

--- speaker_encoder/SpeechEncoder.py
+++ speaker_encoder/SpeechEncoder.py
@@ -1,26 +1,12 @@
 
 import tensorflow as tf
-from speaker_encoder.speaker_encoder_utils import speaker_centroids,utterance_centroids,similarity_matrix,calculate_loss
+from speaker_encoder_utils import speaker_centroids,utterance_centroids,similarity_matrix,calculate_loss
 from keras.layers import LSTM,Dense,Lambda,Masking
+import keras
+import numpy as np
 
 tfk = tf.keras
 tfkl = tfk.layers
-
-
-# def __init__(self, num_layers=3, dim_input=40, dim_cell=256, dim_emb=64):
-    
-# super(D_VECTOR, self).__init__()
-#         self.lstm = nn.LSTM(input_size=dim_input, hidden_size=dim_cell, 
-#                             num_layers=num_layers, batch_first=True)  
-#         self.embedding = nn.Linear(dim_cell, dim_emb)
-
-# def forward(self, x):
-#         self.lstm.flatten_parameters()            
-#         lstm_out, _ = self.lstm(x)
-#         embeds = self.embedding(lstm_out[:,-1,:])
-#         norm = embeds.norm(p=2, dim=-1, keepdim=True) 
-#         embeds_normalized = embeds.div(norm)
-#         return embeds_normalized
 
 
 class SpeechEmbedder(tfk.Model):
